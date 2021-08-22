789d81d8fa44343befc57dbfdbab3a176a32b8f8
Shubham Singh Ranswal <shubhamranswal@gmail.com>
2021-08-22 15:01:27 +0530

    initial commit. created a working project

---
+++ app/src/main/java/com/shubhamranswal/diceroller/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.shubhamranswal.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button= findViewById(R.id.rollButton)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        val resultDiceValue: TextView = findViewById(R.id.diceValue)
+        resultDiceValue.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
