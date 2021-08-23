a8243d443d685015a55c3565ca1e65b3329f5f61
Shubham Singh Ranswal <shubhamranswal@gmail.com>
2021-08-23 15:03:22 +0530

    commit

--- app/src/main/java/com/shubhamranswal/diceroller/MainActivity.kt
+++ app/src/main/java/com/shubhamranswal/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.shubhamranswal.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.internal.ContextUtils.getActivity
@@ -23,10 +24,23 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        val resultDiceValue: TextView = findViewById(R.id.diceValue)
-        resultDiceValue.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.diceValue)
 
-        Toast.makeText(this, "Dice rolled and the value generated", Toast.LENGTH_SHORT).show();
+        // Determine which drawable resource ID to use based on the dice roll
+        val diceResult = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(diceResult)
+
+
+        Toast.makeText(this, "Dice rolled and the value generated $diceRoll", Toast.LENGTH_SHORT).show();
     }
 }
 
